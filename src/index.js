6ded2f95a7f2e391072455be68a9301a313e154e
acevedor <roman.aceved62@gmail.com>
2021-06-18 09:57:15 +0200

    init project

---
+++ src/index.js
@@ -0,0 +1,16 @@
+/**
+ * Background Cloud Function to be triggered by Pub/Sub.
+ * This function is exported by index.js, and executed when
+ * the trigger topic receives a message.
+ * from https://cloud.google.com/functions/docs/calling/pubsub#functions_calling_pubsub-nodejs
+ *
+ * @param {object} message The Pub/Sub message.
+ * @param {object} context The event metadata.
+ */
+ exports.greetFromPubSub = (message, context) => {
+  const name = message.data
+    ? Buffer.from(message.data, 'base64').toString()
+    : 'empty';
+
+  console.log(`Hello I am google-cloud-function-with-terraform, message was: ${name}!`);
+};
